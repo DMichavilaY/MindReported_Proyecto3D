8552ff980d98a41e381654a01271e20c6264d6cf
DMichavilaY <101095899+DMichavilaY@users.noreply.github.com>
2024-02-13 08:37:26 +0100

    13/02/2024

--- Assets/Scripts/SimplePlayerController.cs
+++ Assets/Scripts/SimplePlayerController.cs
@@ -13,43 +13,60 @@ public class SimplePlayerController : MonoBehaviour
     public float lookXLimit = 60.0f;
     public float gravity = 150.0f;
 
-    CharacterController characterController;
-    Vector3 moveDirection = Vector3.zero;
-    float rotationX = 0;
+    private CharacterController characterController;
+    private Vector3 moveDirection = Vector3.zero;
+    private float rotationX = 0;
     private bool canMove = true;
 
     void Start()
     {
+        // Obtener el componente CharacterController
         characterController = GetComponent<CharacterController>();
 
+        // Bloquear el cursor y hacerlo invisible
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
     }
 
     void Update()
     {
+        // Obtener las direcciones hacia adelante y hacia la derecha del personaje
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
+
+        // Verificar si el jugador está corriendo
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        // Calcular la velocidad actual en los ejes X e Y
         float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
         float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+
+        // Guardar la dirección del movimiento antes de aplicar la gravedad
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
+        // Aplicar la gravedad si el personaje no está en el suelo
         if (!characterController.isGrounded)
         {
             moveDirection.y -= gravity * Time.deltaTime;
         }
 
+        // Mover al personaje
         characterController.Move(moveDirection * Time.deltaTime);
 
         if (canMove)
         {
+            // Obtener la entrada del mouse para la rotación en el eje Y
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+
+            // Limitar la rotación en el eje Y
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+
+            // Aplicar la rotación a la cámara localmente
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+
+            // Rotar el personaje en el eje Y
+            transform.Rotate(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
     }
 }

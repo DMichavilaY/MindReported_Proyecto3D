db91fdfcd994943061ab1f0fe6f97d541bb14142
Catalin <64475791+CatalinPurcaras@users.noreply.github.com>
2024-02-15 10:17:57 +0100

    Interactividad Básica con Objetos

---
+++ Assets/Scripts/Scripts Propios/InteractionSystem/Objects.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objects : InteractableObject
+{
+    private Animator doorAnimation;
+    private bool doorOpen = false;
+    private float animationTime;
+    void Awake()
+    {
+        doorAnimation = GetComponent<Animator>();
+    }
+
+    protected override void Interact()
+    {
+        animationTime = doorAnimation.GetCurrentAnimatorStateInfo(0).normalizedTime;
+       if (animationTime >= 1)
+        {
+            if (doorOpen == false)
+            {
+                doorAnimation.PlayInFixedTime("Open");
+                doorOpen = true;
+
+            }
+            else if (doorOpen == true)
+            {
+                doorAnimation.PlayInFixedTime("Close");
+                doorOpen = false;
+            }
+        }
+
+    }
+}

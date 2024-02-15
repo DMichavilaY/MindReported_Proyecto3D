f5d5f9754dd5edf1eafa6f2de24a76d8f9cf4bc7
Catalin <64475791+CatalinPurcaras@users.noreply.github.com>
2024-02-15 10:45:23 +0100

    Interactividad_02

--- Assets/Scripts/Scripts Propios/InteractionSystem/PlayerInteract.cs
+++ Assets/Scripts/Scripts Propios/InteractionSystem/PlayerInteract.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerInteract : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private float distance = 2f;
     [SerializeField] private LayerMask mask;
+    private InteractivePlayerUI playerUI;
 
     void Update()
     {
+        playerUI.UpdateText(string.Empty);
+
         Ray ray = new Ray(cam.transform.position, cam.transform.forward);
         Debug.DrawRay(ray.origin, ray.direction * distance);
         RaycastHit hitInfo;
@@ -19,6 +23,11 @@ public class PlayerInteract : MonoBehaviour
             {
                 InteractableObject interactable = hitInfo.collider.GetComponent<InteractableObject>();
                 interactable.BaseInteract();
+            }
+
+            if (hitInfo.collider.GetComponent<InteractableObject>() != null && hitInfo.collider.tag == "Light")
+            {
+                playerUI.UpdateText(hitInfo.collider.GetComponent<InteractableObject>().promptMessage);
             }
         }
     }

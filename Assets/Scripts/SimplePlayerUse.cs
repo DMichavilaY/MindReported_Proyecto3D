e318dd43ab4698c6509887863284c0aab643980e
DMichavilaY <101095899+DMichavilaY@users.noreply.github.com>
2024-02-15 10:46:37 +0100

    15/02/2024

--- Assets/Scripts/SimplePlayerUse.cs
+++ Assets/Scripts/SimplePlayerUse.cs
@@ -5,60 +5,82 @@ using UnityEngine;
 public class SimplePlayerUse : MonoBehaviour
 {
     public GameObject mainCamera;
-    private GameObject objectClicked;
-    public GameObject flashlight;
     public KeyCode OpenClose;
     public KeyCode Flashlight;
 
-    void Start()
-    {
-
-    }
+    private GameObject objectClicked;
+    private bool flashlightActive = false;
 
     void Update()
     {
         if (Input.GetKeyDown(OpenClose)) // Open and close action
-            {
-                RaycastCheck();
-            }
-
-        if (Input.GetKeyDown(Flashlight)) // Toggle flashlight
         {
-            if (flashlight.activeSelf )
-                  flashlight.SetActive(false);
-            else
-                 flashlight.SetActive(true);
+            RaycastCheck();
+        }
+
+        if (Input.GetKeyDown(Flashlight) && flashlightActive) // Toggle flashlight only if it's active
+        {
+            ToggleFlashlight();
         }
     }
-    
+
     void RaycastCheck()
     {
         RaycastHit hit;
 
         if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward), out hit, 2.3f))
         {
-            if (hit.collider.gameObject.GetComponent<SimpleOpenClose>())
+            GameObject hitObject = hit.collider.gameObject;
+
+            if (hitObject.CompareTag("Flashlight"))
             {
-               // Debug.Log("Object with SimpleOpenClose script found");
-                hit.collider.gameObject.BroadcastMessage("ObjectClicked");
+                ToggleFlashlight();  // Activa/Desactiva el objeto de la linterna
             }
-
-            else
+            else if (hitObject.CompareTag("Flashlight2"))
             {
-               // Debug.Log("Object doesn't have script SimpleOpenClose attached");
-
+                ToggleFlashlight2();  // Activa/Desactiva el objeto con el tag "Flashlight2"
             }
-           // Debug.DrawRay(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-           // Debug.Log("Did Hit");
+            else if (hitObject.GetComponent<SimpleOpenClose>())
+            {
+                hitObject.BroadcastMessage("ObjectClicked");
+            }
         }
-        else
-        {
-         // Debug.DrawRay(mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-         //   Debug.Log("Did not Hit");
-
-
-        }
-
     }
 
+    void ToggleFlashlight()
+    {
+        GameObject[] flashlights = GameObject.FindGameObjectsWithTag("Flashlight");
+
+        if (flashlights.Length > 0)
+        {
+            foreach (GameObject flashlight in flashlights)
+            {
+                flashlight.SetActive(false);  // Desactiva el objeto con el tag "Flashlight"
+            }
+
+            flashlightActive = false;
+        }
+    }
+
+    void ToggleFlashlight2()
+    {
+        GameObject[] flashlights2 = GameObject.FindGameObjectsWithTag("Flashlight2");
+
+        if (flashlights2.Length > 0)
+        {
+            foreach (GameObject flashlight2 in flashlights2)
+            {
+                flashlight2.SetActive(true);  // Activa el objeto con el tag "Flashlight2"
+
+                // Activa el Mesh Renderer si existe
+                MeshRenderer meshRenderer = flashlight2.GetComponent<MeshRenderer>();
+                if (meshRenderer != null)
+                {
+                    meshRenderer.enabled = true;
+                }
+            }
+
+            flashlightActive = true;
+        }
+    }
 }

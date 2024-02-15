0a170e0ff59c0738381673ce8ffc1ed422b163ee
Catalin <64475791+CatalinPurcaras@users.noreply.github.com>
2024-02-15 08:53:55 +0100

    Nuevo LayerMask

---
+++ Assets/Scripts/Scripts Propios/InteractionSystem/PlayerInteract.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private float distance = 2f;
+    [SerializeField] private LayerMask mask;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

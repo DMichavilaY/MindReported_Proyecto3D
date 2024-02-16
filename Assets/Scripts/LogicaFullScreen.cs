3e0918617a9ce17a49ac1a4885c14fa094723b5f
Touhafi <151826423+Touhafi@users.noreply.github.com>
2024-02-16 13:28:05 +0100

    16.02.24

--- Assets/Scripts/LogicaFullScreen.cs
+++ Assets/Scripts/LogicaFullScreen.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LogicaFullScreen : MonoBehaviour
 {
-    public Toggle toggle;
+   
+     public Toggle toggle;
+    public TMP_Dropdown resolucionesDropDown;
+    Resolution[] resoluciones;
 
-    // Start is called before the first frame update
     void Start()
     {
         if (Screen.fullScreen)
@@ -18,15 +21,46 @@ public class LogicaFullScreen : MonoBehaviour
         {
             toggle.isOn = false;
         }
+
+        RevisarResolucion();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Aquí puedes agregar lógica de actualización si es necesario
     }
 
     public void ActivarPantallaCompleta(bool pantallaCompleta)
     {
         Screen.fullScreen = pantallaCompleta;
+    }
+
+    public void RevisarResolucion()
+    {
+        resoluciones = Screen.resolutions;
+        resolucionesDropDown.ClearOptions();
+        List<string> opciones = new List<string>();
+        int resolucionActual = 0;
+
+        for (int i = 0; i < resoluciones.Length; i++)
+        {
+            string opcion = resoluciones[i].width + "x" + resoluciones[i].height;
+            opciones.Add(opcion);
+
+            if (Screen.fullScreen && resoluciones[i].width == Screen.currentResolution.width && resoluciones[i].height == Screen.currentResolution.height)
+            {
+                resolucionActual = i;
+            }
+        }
+
+        resolucionesDropDown.AddOptions(opciones);
+        resolucionesDropDown.value = resolucionActual;
+        resolucionesDropDown.RefreshShownValue();
+    }
+
+    public void CambiarResolucion(int indiceResolucion)
+    {
+        Resolution resolucion = resoluciones[indiceResolucion];
+        Screen.SetResolution(resolucion.width, resolucion.height, Screen.fullScreen);
     }
 }

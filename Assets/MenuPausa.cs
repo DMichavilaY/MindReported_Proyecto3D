6d49aa69e0c5b1900bcc23ae147e731d9d29e927
Touhafi <151826423+Touhafi@users.noreply.github.com>
2024-02-19 14:48:50 +0100

    19-02-24

---
+++ Assets/MenuPausa.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPausa : MonoBehaviour
+
+{
+    public GameObject ObjetoMenuPausa;
+    public bool Pausa = false;
+
+    void Start()
+    {
+        // Aquí puedes inicializar variables si es necesario
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Pausa== false)
+            {
+            ObjetoMenuPausa.SetActive(true);
+            Pausa = true;
+
+            Time.timeScale = 0;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            }
+           
+        }
+
+  
+    }
+
+    public void Resumir()
+    {
+        ObjetoMenuPausa.SetActive(false);
+        Pausa = false;
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}

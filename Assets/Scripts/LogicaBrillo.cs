960556fad48cad7ea3cc4807b6a035b9bf2a3045
DMichavilaY <101095899+DMichavilaY@users.noreply.github.com>
2024-02-23 13:24:46 +0100

    23/02/2024

--- Assets/Scripts/LogicaBrillo.cs
+++ Assets/Scripts/LogicaBrillo.cs
@@ -6,27 +6,28 @@ using UnityEngine.UI;
 public class LogicaBrillo : MonoBehaviour
 {
     public Slider slider;
-    public float sliderValue;
+    public float brilloMinimo = 0.5f; // Ajusta este valor según tu preferencia
+    public float brilloMaximo = 0.8f; // Ajusta este valor según tu preferencia
     public Image panelBrillo;
 
     // Start is called before the first frame update
     void Start()
     {
-        slider.value = PlayerPrefs.GetFloat("brillo", 0.5f);
+        slider.value = PlayerPrefs.GetFloat("brillo", brilloMaximo); // Establece el valor predeterminado en el brillo máximo permitido
         panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, slider.value);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Aquí no hay código en el método Update. Si necesitas algo que se actualice cada frame, agregalo aquí.
+        // Aquí no hay código en el método Update. Si necesitas algo que se actualice cada frame, agrégalo aquí.
     }
 
     // Este método se llama cuando se cambia el valor del slider
     public void ChangeSlider(float valor)
     {
-        sliderValue = valor;
-        PlayerPrefs.SetFloat("brillo", sliderValue);
+        slider.value = Mathf.Clamp(valor, brilloMinimo, brilloMaximo); // Limita el valor entre brilloMinimo y brilloMaximo
+        PlayerPrefs.SetFloat("brillo", slider.value);
         panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, slider.value);
     }
 }

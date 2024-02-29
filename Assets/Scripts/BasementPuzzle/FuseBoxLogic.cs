9d3248e3bfeb782b583c8538d4696b6dda0ef029
Catalin <catapurcaras@gmail.com>
2024-02-29 18:18:27 +0100

    KeyPadMaterials

--- Assets/Scripts/BasementPuzzle/FuseBoxLogic.cs
+++ Assets/Scripts/BasementPuzzle/FuseBoxLogic.cs
@@ -8,6 +8,7 @@ public class FuseBoxLogic : MonoBehaviour
     [SerializeField] private Collider basementDoor;
     [SerializeField] private Animator doorAnimator;
     [SerializeField] private AudioSource doorClosing;
+    [SerializeField] private GameObject lights;
 
     private Collider fuseBox;
 
@@ -21,11 +22,14 @@ public class FuseBoxLogic : MonoBehaviour
         doorAnimator.Play("Close");
         basementDoor.enabled = false;
         doorClosing.Play();
+        StartCoroutine(DisableScript());
     }
 
     IEnumerator DisableScript()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);
+        lights.SetActive(true);
+        yield return new WaitForSeconds(1);
         fuseBox.enabled = false;
        
     }
